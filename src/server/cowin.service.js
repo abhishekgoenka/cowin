6d7b9ce2313f6b3844efa65d20323ed9190389b5
Abhishek Goenka <abhishek.goenka@epicor.com>
2021-05-10 12:15:43 +0530

    Update cowin.service.js

--- src/server/cowin.service.js
+++ src/server/cowin.service.js
@@ -1,7 +1,7 @@
 const https = require("https");
 
 function getCenters(req, res) {
-  const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=505&date=09-05-2021`;
+  const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=505&date=10-05-2021`;
   console.log(url);
   https.get(url, (resp) => {
     let data = "";
@@ -14,11 +14,16 @@ function getCenters(req, res) {
     // The whole response has been received. Print out the result.
     resp
       .on("end", () => {
-        // console.log(JSON.parse(data));
-        res.status(200).json(JSON.parse(data));
+        if (data && data != 'Unauthenticated access!') {
+          res.status(200).json(JSON.parse(data));
+        } else {
+          console.log('response : ' + data);
+          res.status(500).json(data);
+        }
       })
       .on("error", (err) => {
         console.log("Error: " + err.message);
+        res.status(500).json(err.message);
       });
   });
 }
